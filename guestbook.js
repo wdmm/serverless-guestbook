7a815e726673b0a954a3ab7fe15093b2fbcd6ab4
Wdm M <13vadimm@gmail.com>
2022-06-29 10:58:37 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://0ef7e01b.eu-de.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
